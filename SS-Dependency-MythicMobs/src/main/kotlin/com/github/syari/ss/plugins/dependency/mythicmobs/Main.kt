23a6d77af9b54a3fa9b82d56ab3e3a24c43b4533
sya-ri <sya79lua@gmail.com>
2020-12-14 00:40:09 +0900

    [Dependency-*] Main::onEnableを削除

--- SS-Dependency-MythicMobs/src/main/kotlin/com/github/syari/ss/plugins/dependency/mythicmobs/Main.kt
+++ SS-Dependency-MythicMobs/src/main/kotlin/com/github/syari/ss/plugins/dependency/mythicmobs/Main.kt
@@ -2,8 +2,4 @@ package com.github.syari.ss.plugins.dependency.mythicmobs
 
 import com.github.syari.ss.plugins.core.code.SSPlugin
 
-class Main: SSPlugin() {
-    override fun onEnable() {
-
-    }
-}
+class Main: SSPlugin()
