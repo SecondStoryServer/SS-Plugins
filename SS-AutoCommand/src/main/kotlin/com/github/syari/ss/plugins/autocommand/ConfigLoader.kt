9daa27e58e37e6f8a4f50ac672fb9f0ebc23c1de
sya-ri <sya79lua@gmail.com>
2021-01-03 01:08:34 +0900

    [AutoCommand] 自動でコマンドが実行されるようにした

---
+++ SS-AutoCommand/src/main/kotlin/com/github/syari/ss/plugins/autocommand/ConfigLoader.kt
@@ -0,0 +1,54 @@
+package com.github.syari.ss.plugins.autocommand
+
+import com.github.syari.ss.plugins.autocommand.Main.Companion.plugin
+import com.github.syari.ss.plugins.core.Main.Companion.console
+import com.github.syari.ss.plugins.core.code.OnEnable
+import com.github.syari.ss.plugins.core.command.RunCommand
+import com.github.syari.ss.plugins.core.command.RunCommand.runCommandFromConsole
+import com.github.syari.ss.plugins.core.config.CreateConfig.config
+import com.github.syari.ss.plugins.core.config.dataType.ConfigDataType
+import com.github.syari.ss.plugins.core.message.Message.send
+import com.github.syari.ss.plugins.core.time.TimeScheduler
+import org.bukkit.command.CommandSender
+import java.time.DayOfWeek
+
+object ConfigLoader: OnEnable {
+    override fun onEnable() {
+        load(console)
+    }
+
+    fun load(sender: CommandSender) {
+        fun String.toTime() = split(":").let { it.getOrNull(0)?.toIntOrNull() to it.getOrNull(1)?.toIntOrNull() }
+
+        plugin.config(sender, "config.yml") {
+            sender.send("&b[AutoCommand] &f自動コマンド一覧")
+            section("every")?.forEach {
+                val commandList = get("every.$it", ConfigDataType.STRINGLIST) ?: return@forEach
+                val (hour, minute) = it.toTime()
+                if (hour != null && minute != null) {
+                    TimeScheduler.scheduleEveryDayAt(hour, minute) {
+                        commandList.forEach(RunCommand::runCommandFromConsole)
+                    }
+                    sender.send("&7- &a$hour:$minute &7×${commandList.size}")
+                } else {
+                    formatMismatchError("every.$it")
+                }
+            }
+            DayOfWeek.values().forEach { day ->
+                val dayName = day.name.toLowerCase()
+                section(dayName)?.forEach nextTime@{
+                    val commandList = get("$dayName.$it", ConfigDataType.STRINGLIST) ?: return@nextTime
+                    val (hour, minute) = it.toTime()
+                    if (hour != null && minute != null) {
+                        TimeScheduler.scheduleEveryWeekAt(day, hour, minute) {
+                            commandList.forEach(RunCommand::runCommandFromConsole)
+                        }
+                        sender.send("&7- &a$dayName $hour:$minute &7×${commandList.size}")
+                    } else {
+                        formatMismatchError("$dayName.$it")
+                    }
+                }
+            }
+        }
+    }
+}
