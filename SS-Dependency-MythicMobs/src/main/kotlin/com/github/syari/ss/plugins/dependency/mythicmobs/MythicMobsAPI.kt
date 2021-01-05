175d304cc10f318961de77799b326baa1dab08a4
sya-ri <sya79lua@gmail.com>
2021-01-05 23:24:19 +0900

    [Dependency-MythicMobs] spawnMythicMobsを追加

--- SS-Dependency-MythicMobs/src/main/kotlin/com/github/syari/ss/plugins/dependency/mythicmobs/MythicMobsAPI.kt
+++ SS-Dependency-MythicMobs/src/main/kotlin/com/github/syari/ss/plugins/dependency/mythicmobs/MythicMobsAPI.kt
@@ -2,11 +2,17 @@ package com.github.syari.ss.plugins.dependency.mythicmobs
 
 import com.github.syari.ss.plugins.core.item.CustomItemStack
 import io.lumine.xikage.mythicmobs.MythicMobs
+import io.lumine.xikage.mythicmobs.mobs.ActiveMob
+import org.bukkit.Location
 
 object MythicMobsAPI {
     private val api = MythicMobs.inst()
 
     fun getItem(id: String, amount: Int): CustomItemStack? {
         return CustomItemStack.fromNullable(api.itemManager.getItemStack(id), amount)
+    }
+
+    fun spawnMythicMobs(id: String, location: Location): ActiveMob? {
+        return api.mobManager.spawnMob(id, location)
     }
 }
