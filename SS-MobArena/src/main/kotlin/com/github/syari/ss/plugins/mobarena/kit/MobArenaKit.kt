3c64f11aaeed4ebb3d64110d69db691c928932a0
sya-ri <sya79lua@gmail.com>
2021-01-20 20:33:12 +0900

    [MobArena] キット一覧のGUIを追加 #114

--- SS-MobArena/src/main/kotlin/com/github/syari/ss/plugins/mobarena/kit/MobArenaKit.kt
+++ SS-MobArena/src/main/kotlin/com/github/syari/ss/plugins/mobarena/kit/MobArenaKit.kt
@@ -1,13 +1,66 @@
 package com.github.syari.ss.plugins.mobarena.kit
 
+import com.github.syari.ss.plugins.core.inventory.CreateInventory.inventory
+import com.github.syari.ss.plugins.core.item.CustomItemStack
+import com.github.syari.ss.plugins.mobarena.MobArenaManager
+import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
-class MobArenaKit(val id: String, val name: String, val items: Map<Int, ItemStack>) {
+class MobArenaKit(
+    val id: String,
+    val name: String,
+    icon: CustomItemStack,
+    description: String,
+    difficulty: Int,
+    val items: Map<Int, ItemStack>
+) {
     companion object {
         var kits = mapOf<String, MobArenaKit>()
+            set(value) {
+                field = value
+                kitPage = kits.values.toList().chunked(45)
+            }
+
+        private lateinit var kitPage: List<List<MobArenaKit>>
 
         fun getKit(id: String?) = kits[id]
+
+        fun openPreviewList(player: Player, page: Int = 0) {
+            val lastPage = kitPage.lastIndex
+            when {
+                page < 0 -> openPreviewList(player, 0)
+                lastPage < page -> openPreviewList(player, lastPage)
+                else -> inventory("&9&lキット一覧 ($page/$lastPage)", 6) {
+                    kitPage[page].forEachIndexed { i, kit ->
+                        item(
+                            i,
+                            kit.icon.clone {
+                                editLore {
+                                    add("")
+                                    add("&a使用アリーナ")
+                                    addAll(MobArenaManager.arenas.filter { it.kits.contains(kit.id) }.map { "&7- ${it.id}" })
+                                }
+                            }
+                        ).event {
+                            kit.openPreview(player) {
+                                openPreviewList(player, page)
+                            }
+                        }
+                    }
+                    item(45..53, Material.GRAY_STAINED_GLASS_PANE)
+                    item(47, Material.ORANGE_STAINED_GLASS_PANE, "&d<<").event {
+                        openPreviewList(player, page - 1)
+                    }
+                    item(51, Material.ORANGE_STAINED_GLASS_PANE, "&d>>").event {
+                        openPreviewList(player, page + 1)
+                    }
+                    item(49, Material.RED_STAINED_GLASS_PANE, "&c&l閉じる").event {
+                        player.closeInventory()
+                    }
+                }.open(player)
+            }
+        }
     }
 
     fun load(player: Player) {
@@ -16,5 +69,35 @@ class MobArenaKit(val id: String, val name: String, val items: Map<Int, ItemStac
         items.forEach { (slot, item) ->
             player.inventory.setItem(slot, item)
         }
+    }
+
+    val icon = icon.clone {
+        display = name
+        editLore {
+            add(description)
+            add("")
+            add("&6" + "⭐".repeat(difficulty))
+        }
+    }
+
+    fun openPreview(player: Player, onClose: () -> Unit) {
+        inventory(name, 6) {
+            var slot = 0
+            for (i in 9 until 36) {
+                items[i]?.let { item(slot, it) }
+                slot ++
+            }
+            for (i in 0 until 9) {
+                items[i]?.let { item(slot, it) }
+                slot ++
+            }
+            slot += 2
+            for (i in 36..40) {
+                items[i]?.let { item(slot, it) }
+                slot ++
+            }
+            item((36..37) + (43..48) + (50..53), Material.GRAY_STAINED_GLASS_PANE)
+            item(49, Material.RED_STAINED_GLASS_PANE, "&c&l閉じる").event(onClose)
+        }.open(player)
     }
 }
