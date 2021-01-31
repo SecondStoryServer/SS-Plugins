20a666da97a14fd76482003bebb02b93ea943e64
sya-ri <sya79lua@gmail.com>
2021-02-01 01:01:06 +0900

    [Lobby] 一度使用したら花火がなくなる問題を修正

--- SS-Lobby/src/main/kotlin/com/github/syari/ss/plugins/lobby/Main.kt
+++ SS-Lobby/src/main/kotlin/com/github/syari/ss/plugins/lobby/Main.kt
@@ -1,11 +1,17 @@
 package com.github.syari.ss.plugins.lobby
 
 import com.github.syari.ss.plugins.core.code.SSPlugin
+import org.bukkit.plugin.java.JavaPlugin
 
 class Main : SSPlugin() {
+    companion object {
+        internal lateinit var plugin: JavaPlugin
+    }
+
     override val events = listOf(EventListener)
 
     override fun onEnable() {
+        plugin = this
         registerEvents()
     }
 }
--- SS-Lobby/src/main/kotlin/com/github/syari/ss/plugins/lobby/gadget/Elytra.kt
+++ SS-Lobby/src/main/kotlin/com/github/syari/ss/plugins/lobby/gadget/Elytra.kt
@@ -1,6 +1,10 @@
 package com.github.syari.ss.plugins.lobby.gadget
 
 import com.github.syari.ss.plugins.core.item.CustomItemStack
+import com.github.syari.ss.plugins.core.player.UUIDPlayer
+import com.github.syari.ss.plugins.core.scheduler.CreateScheduler.runTimer
+import com.github.syari.ss.plugins.core.scheduler.CustomTask
+import com.github.syari.ss.plugins.lobby.Main.Companion.plugin
 import org.bukkit.Material
 import org.bukkit.entity.Player
 
@@ -17,15 +21,22 @@ object Elytra : Gadget(Material.ELYTRA, "エリトラ", "lobby.gadget.elytra") {
         "&d花火"
     ).toOneItemStack
 
+    private val giveFireworkTask = mutableMapOf<UUIDPlayer, CustomTask>()
+
     override fun onEnable(player: Player, itemStack: CustomItemStack) {
         player.inventory.chestplate = elytra
-        player.inventory.setItemInOffHand(fireworkRocket)
+        plugin.runTimer(0, 20) {
+            player.inventory.setItemInOffHand(fireworkRocket)
+        }?.let {
+            giveFireworkTask[UUIDPlayer(player)] = it
+        }
         super.onEnable(player, itemStack)
     }
 
     override fun onDisable(player: Player, itemStack: CustomItemStack) {
         player.inventory.chestplate = null
         player.inventory.setItemInOffHand(null)
+        giveFireworkTask.remove(UUIDPlayer(player))?.cancel()
         super.onDisable(player, itemStack)
     }
 }
