7acb0bed5b5b74791a9231aa3f61159f1dcfcfc5
sya-ri <sya79lua@gmail.com>
2021-02-12 22:57:00 +0900

    [Core] EasySpigotAPI を v1.2.1 に更新

--- SS-Core/build.gradle.kts
+++ SS-Core/build.gradle.kts
@@ -4,7 +4,7 @@ group = project.group
 dependencies {
     shadowImplementation(kotlin("stdlib-jdk8"))
     api(files("dependencyJar/patched_1.16.5.jar"))
-    shadowApi("com.github.sya-ri:EasySpigotAPI:1.2.0") {
+    shadowApi("com.github.sya-ri:EasySpigotAPI:1.2.1") {
         exclude(group = "org.spigotmc", module = "spigot-api")
         exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib")
     }
