2f5c9151e26879a3a93ef031f8944d30dee3e672
sya-ri <sya79lua@gmail.com>
2021-02-17 21:18:03 +0900

    [Event-AcrobatSniper] ダブルジャンプの挙動を追加

--- SS-Event-AcrobatSniper/src/main/kotlin/com/github/syari/ss/plugins/event/acrobatsniper/Main.kt
+++ SS-Event-AcrobatSniper/src/main/kotlin/com/github/syari/ss/plugins/event/acrobatsniper/Main.kt
@@ -2,4 +2,10 @@ package com.github.syari.ss.plugins.event.acrobatsniper
 
 import com.github.syari.ss.plugins.core.code.SSPlugin
 
-class Main : SSPlugin()
+class Main : SSPlugin() {
+    override val events = listOf(DoubleJump)
+
+    override fun onEnable() {
+        registerEvents()
+    }
+}
