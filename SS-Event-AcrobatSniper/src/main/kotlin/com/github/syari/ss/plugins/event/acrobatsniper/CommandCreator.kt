205471493a7cf4de3406992b56558042f73e112f
sya-ri <sya79lua@gmail.com>
2021-02-19 18:48:42 +0900

    [AcrobatSniper] エントリーの処理を追加

--- SS-Event-AcrobatSniper/src/main/kotlin/com/github/syari/ss/plugins/event/acrobatsniper/CommandCreator.kt
+++ SS-Event-AcrobatSniper/src/main/kotlin/com/github/syari/ss/plugins/event/acrobatsniper/CommandCreator.kt
@@ -2,8 +2,12 @@ package com.github.syari.ss.plugins.event.acrobatsniper
 
 import com.github.syari.spigot.api.command.command
 import com.github.syari.spigot.api.command.tab.CommandTabArgument.Companion.argument
+import com.github.syari.spigot.api.util.uuid.UUIDPlayer
 import com.github.syari.ss.plugins.core.code.OnEnable
 import com.github.syari.ss.plugins.core.command.getPlayer
+import com.github.syari.ss.plugins.core.message.JsonBuilder
+import com.github.syari.ss.plugins.core.message.JsonBuilder.Companion.buildJson
+import com.github.syari.ss.plugins.core.message.template.ConstantMessage.OnlyPlayer
 import com.github.syari.ss.plugins.core.message.template.ConstantMessage.ReloadConfig
 import com.github.syari.ss.plugins.core.message.template.templateMessage
 import com.github.syari.ss.plugins.event.acrobatsniper.Main.Companion.plugin
@@ -11,12 +15,44 @@ import org.bukkit.entity.Player
 
 object CommandCreator : OnEnable {
     override fun onEnable() {
-        plugin.command("acrobatsniper") {
+        plugin.command("acrobat-sniper") {
             aliases = listOf("as")
             tab {
-                argument { add("match", "reload") }
+                argument { add("entry") }
+            }
+            execute {
+                val template = templateMessage("AcrobatSniper")
+                val player = sender as? Player ?: return@execute template.sendError(OnlyPlayer)
+                when (args.lowerOrNull(0)) {
+                    "entry" -> {
+                        if (EntryList.isEnable.not()) {
+                            return@execute template.sendError("エントリー受付時間外です")
+                        }
+                        val uuidPlayer = UUIDPlayer.from(player)
+                        if (EntryList.contains(uuidPlayer)) {
+                            EntryList.remove(uuidPlayer)
+                            template.send("エントリーを取り消しました")
+                        } else {
+                            EntryList.add(uuidPlayer)
+                            template.send("エントリーしました")
+                        }
+                    }
+                    else -> {
+                        template.sendCommandHelp(
+                            "$label entry" to "参加登録を行います"
+                        )
+                    }
+                }
+            }
+        }
+        plugin.command("acrobat-sniper-admin") {
+            aliases = listOf("asa")
+            tab {
+                argument { add("match", "entry", "reload") }
                 argument("match") { add("test") }
                 argument("match test **") { addAll(plugin.server.onlinePlayers.map(Player::getName)) }
+                argument("entry") { add("list", "clear", "enable", "disable") }
+                argument("entry list") { add("all") }
             }
             execute {
                 val template = templateMessage("AcrobatSniper")
@@ -32,6 +68,41 @@ object CommandCreator : OnEnable {
                                 template.sendCommandHelp(
                                     "$label match test [Player] [Player]" to "対戦のテストを行います"
                                 )
+                            }
+                        }
+                    }
+                    "entry" -> {
+                        when (args.lowerOrNull(1)) {
+                            "list" -> {
+                                val nameList = EntryList.nameList
+                                template.send(
+                                    buildJson {
+                                        append(
+                                            "エントリー一覧",
+                                            JsonBuilder.Hover.Text("&6コピー"),
+                                            JsonBuilder.Click.Clipboard(nameList.joinToString("\n"))
+                                        )
+                                        val nameListLimit = if (args.lowerOrNull(2) == "all") {
+                                            -1
+                                        } else {
+                                            args.getOrNull(2)?.toIntOrNull() ?: 5
+                                        }
+                                        append(nameList.joinToString(limit = nameListLimit) { "&7$it" })
+                                        append(" &a(${nameList.size})")
+                                    }
+                                )
+                            }
+                            "clear" -> {
+                                EntryList.clear()
+                                template.send("エントリーを削除しました")
+                            }
+                            "enable" -> {
+                                EntryList.isEnable = true
+                                template.send("エントリー受付を開始します")
+                            }
+                            "disable" -> {
+                                EntryList.isEnable = false
+                                template.send("エントリー受付を終了します")
                             }
                         }
                     }
---
+++ SS-Event-AcrobatSniper/src/main/kotlin/com/github/syari/ss/plugins/event/acrobatsniper/EntryList.kt
@@ -0,0 +1,28 @@
+package com.github.syari.ss.plugins.event.acrobatsniper
+
+import com.github.syari.spigot.api.util.uuid.UUIDPlayer
+
+object EntryList {
+    private val list = mutableSetOf<UUIDPlayer>()
+
+    val nameList
+        get() = list.mapNotNull { it.offlinePlayer.name }
+
+    var isEnable = false
+
+    fun contains(player: UUIDPlayer): Boolean {
+        return list.contains(player)
+    }
+
+    fun add(player: UUIDPlayer) {
+        list.add(player)
+    }
+
+    fun remove(player: UUIDPlayer) {
+        list.remove(player)
+    }
+
+    fun clear() {
+        list.clear()
+    }
+}
