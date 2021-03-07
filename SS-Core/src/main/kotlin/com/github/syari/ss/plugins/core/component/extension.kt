18d1519897b4a63190d542bed7c4544668834cf5
sya-ri <sya79lua@gmail.com>
2021-03-08 03:34:29 +0900

    [Core] EasySpigotAPI を v1.8.0 に変更

--- SS-Core/src/main/kotlin/com/github/syari/ss/plugins/core/component/extension.kt
+++
@@ -1,9 +0,0 @@
-package com.github.syari.ss.plugins.core.component
-
-import com.github.syari.ss.plugins.core.item.NBTItem.nbtTag
-import net.md_5.bungee.api.chat.HoverEvent
-import net.md_5.bungee.api.chat.ItemTag
-import net.md_5.bungee.api.chat.hover.content.Item
-import org.bukkit.inventory.ItemStack
-
-fun hoverItem(item: ItemStack) = HoverEvent(HoverEvent.Action.SHOW_ITEM, Item(item.type.key.key, 1, ItemTag.ofNbt(item.nbtTag)))
